7f66949317ca8b7fc11ba6001edb01b4b7e3a14b
zosh100 <zoshmchunu@gmail.com>
2025-03-03 11:32:09 +0200

    Major tweaks

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NtandoMabuthoStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult StudentDisplay()
+        {
+            List<Models.Student> students = new List<Models.Student>();
+            {
+                students.Add(new Models.Student { StudentNumber = 01234567, FirstNaam = "Jane", Surname = "Doe", Email = "jane.doe@tuks.co.za" });
+                students.Add(new Models.Student { StudentNumber = 12345678, FirstNaam = "John", Surname = "Doe", Email = "john.doe@tuks.co.za" });
+                students.Add(new Models.Student { StudentNumber = 23717662, FirstNaam = "Leo", Surname = "Mchunu", Email = "mchunuleo@tuks.co.za" });
+                students.Add(new Models.Student { StudentNumber = 29695682, FirstNaam = "Cosmo", Surname = "Macigwane", Email = "cosmology@gmail.com" });
+                students.Add(new Models.Student { StudentNumber = 28779832, FirstNaam = "Kenneth", Surname = "Ngubane", Email = "kenniengubane@gmail.com" });
+
+
+                return View(students);
+            }
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NtandoMabuthoStudyGroup.Models
+{
+	public class Student
+	{
+        
+            [Display(Name = "STUDENT NUMBER")]
+            public int StudentNumber { get; set; }
+
+            [Display(Name = "NAME")]
+            public string FirstNaam{ get; set; }
+
+            [Display(Name = "SURNAME")]
+            public string Surname { get; set; }
+
+            [Display(Name = "EMAIL ADDRESS")]
+            public string Email { get; set; }
+
+      
+    }
+}
